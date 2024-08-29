0444fc86da36be8c12e49dc4b020d687bab84f2d
Arlensh <lucianobarrerapaez@gmail.com>
2024-08-29 21:32:24 +0200

    Se suben todos los cambios para la visualización de la tabla llamando a la api de countries

---
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'shared-search-box',
+  templateUrl: './search-box.component.html',
+})
+export class SearchBoxComponent {
+  @Output()
+  onValue = new EventEmitter<string>();
+
+  @Input()
+  public placeholder: string = '';
+
+  emitValue(value: string) {
+    this.onValue.emit(value);
+  }
+}
